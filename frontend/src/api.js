2eb519f65787a84bfca497307371ff92e7ee9d6c
kelda-web <kelmarry13@gmail.com>
2026-06-26 04:25:02 +0530

    Set API base URL to local backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+    baseURL: "http://localhost:5000" // Unga backend server port 5000 la run aana idhu okay!
 });
 
 export default API;
